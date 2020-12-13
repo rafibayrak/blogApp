c1fe745da31bc2be16b4b2dd28df3fc5b1639aba
rafi <rafi_bayrak@hotmail.com>
2020-12-13 22:08:19 +0300

    authentication eklendi

--- BlogApplication/Startup.cs
+++ BlogApplication/Startup.cs
@@ -1,11 +1,13 @@
-using AutoMapper;
+﻿using AutoMapper;
 using BlogApplication.Core.IRepositories;
 using BlogApplication.Core.IServices;
 using BlogApplication.Core.IUnitOfWorks;
 using BlogApplication.Data;
 using BlogApplication.Data.Repositories;
 using BlogApplication.Data.UnitOfWorks;
+using BlogApplication.DataContact;
 using BlogApplication.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Features;
@@ -14,7 +16,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Text;
 using System.Text.Json;
 
 namespace BlogApplication
@@ -62,6 +66,39 @@ namespace BlogApplication
                 options.BufferBodyLengthLimit = Int32.MaxValue;
                 options.MultipartBodyLengthLimit = Int32.MaxValue;
             });
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // Oluşturduğumuz gizli anahtarımızı byte dizisi olarak alıyoruz.
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.SecretKey);
+
+            //Projede farklı authentication tipleri olabileceği için varsayılan olarak JWT ile kontrol edeceğimizin bilgisini kaydediyoruz.
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                //JWT kullanacağım ve ayarları da şunlar olsun dediğimiz yer ise burasıdır.
+                .AddJwtBearer(x =>
+                {
+                    //Gelen isteklerin sadece HTTPS yani SSL sertifikası olanları kabul etmesi(varsayılan true)
+                    x.RequireHttpsMetadata = false;
+                    //Eğer token onaylanmış ise sunucu tarafında kayıt edilir.
+                    x.SaveToken = true;
+                    //Token içinde neleri kontrol edeceğimizin ayarları.
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        //Token 3.kısım(imza) kontrolü
+                        ValidateIssuerSigningKey = true,
+                        //Neyle kontrol etmesi gerektigi
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        //Bu iki ayar ise "aud" ve "iss" claimlerini kontrol edelim mi diye soruyor
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -86,6 +123,10 @@ namespace BlogApplication
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
